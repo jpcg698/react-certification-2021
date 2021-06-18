f49104c9305cbc39d2ae1a006b597c72f84cc549
Juan Pablo <juanpcortesg@gmail.com>
2021-06-18 14:32:52 -0500

    added youtube api to search videos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import VideoCard from "./components/videocard/videocard.jsx"
 //import "./css/index.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import js from "../src/data/yt";
-
+import { useEffect, useState } from "react";
 
 const GridWrapper = styled.div`
 display:grid;
@@ -25,22 +25,72 @@ border:1px solid black
 `
 
 
-const VideoList = js.items.filter(vid => (vid.id.kind === "youtube#video")).map((vid)=>(
-  <Cards key={vid.id.videoId} className="cards">
-    <VideoCard 
-    thumbnail={vid.snippet.thumbnails.high.url} 
-    title={vid.snippet.title} url={vid.id.videoId} 
-    description={vid.snippet.description}
-    kind={vid.id.kind}
-    />
-  </Cards>
-));
-
-
 function App() {
+  const [data,setData] = useState(js.items)
+  var VideoList = data.filter(vid => (vid.id.kind === "youtube#video")).map((vid)=>(
+    <Cards key={vid.id.videoId} className="cards">
+      <VideoCard 
+      thumbnail={vid.snippet.thumbnails.high.url} 
+      title={vid.snippet.title} url={vid.id.videoId} 
+      description={vid.snippet.description}
+      kind={vid.id.kind}
+      />
+    </Cards>
+  ));
+
+  // function start() {
+  //   // 2. Initialize the JavaScript client library.
+  //   window.gapi.client.init({
+  //     'apiKey': "AIzaSyDHYuXbei8FcFwG52FQS6_Ad66aH0e5YHA",
+  //     // // clientId and scope are optional if auth is not required.
+  //     // 'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+  //     // 'scope': 'profile',
+  //   }).then(function() {
+  //     // 3. Initialize and make the API request.
+  //     return window.gapi.client.request({
+  //       'path': `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=Wizeline`,
+  //     })
+  //   }).then(function(response) {
+  //     console.log(response.result);
+  //     setData(response.result.items)
+  //   }, function(reason) {
+  //     console.log('Error: ' + reason.result.error.message);
+  //   });
+  // };
+  
+  function start(q="Wizeline") {
+    // 2. Initialize the JavaScript client library.
+    window.gapi.client.init({
+      'apiKey': "AIzaSyDHYuXbei8FcFwG52FQS6_Ad66aH0e5YHA",
+      // // clientId and scope are optional if auth is not required.
+      // 'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+      // 'scope': 'profile',
+    })
+  };
+
+  function execute(q) {
+    // 3. Initialize and make the API request.
+    return window.gapi.client.request({
+      'path': `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${q}`,
+    }).then(function(response) {
+    console.log(response.result);
+    console.log({q})
+    setData(response.result.items)
+  }, function(reason) {
+    console.log('Error: ' + reason.result.error.message);
+  });
+  }
+  
+  useEffect(() => {
+    window.gapi.load('client', start);
+    console.log("Client loaded")
+  }, []);
+
+
+
   return (
     <div>
-        <Header/>
+        <Header passSearch={searchVal =>execute(searchVal)}/>
       <GridWrapper>
           {VideoList}
       </GridWrapper>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,24 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import {Navbar,Button,Form,FormControl} from "react-bootstrap";
 
-function Header(props){
+function Header({passSearch}){
+
+    const [search,setSearch] = useState("Wizeline")
+    
+
+
     return(
             <Navbar bg="primary" variant ="dark">
-                <Navbar.Brand href="#" >
-                <Form inline>
-                    <FormControl type="text" placeholder="Wizeline" className=" mr-sm-2" />
-                </Form>
+                <Navbar.Brand>
+                <form onSubmit={(event)=> {
+                    event.preventDefault()
+                    passSearch(search)}}>
+                    <FormControl type="text" placeholder="Wizeline" className=" mr-sm-2" onChange={(e)=> setSearch(e.target.value)} />
+                </form>
                 </Navbar.Brand>
-                
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text id="darmode">
                             Darkmode  
                     </Navbar.Text>
-                   <from inline>
+                   <Form inline>
                         <Button id="Login">
                             Login
                         </Button>
-                    </from>
+                    </Form>
                     
                 </Navbar.Collapse>
             </Navbar>
